d5d57d603964aeb7b782b5c15d0396a5af8e0178
yuto-miyahara <yt.miyahara@gmail.com>
2024-04-08 00:13:25 +0900

    Nuxtの開発サーバーの設定を変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true,
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 })
