2bcf54100cc6888d9b316f52000c4c9347c21002
yuto-miyahara <yt.miyahara@gmail.com>
2024-04-17 16:41:19 +0900

    プロフィールページを仮作成

---
+++ components/breadcrumb.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps({
+  bcParam: Array as () => {
+    text: string,
+    path: string | null,
+  }[],
+})
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">トップページ</NuxtLink>
+      </li>
+
+      <template v-for="item, index in bcParam">
+        <li v-if="item.text">
+          <span v-if="!item.path || index === (bcParam?.length ?? 0) - 1">{{ item.text }}</span>
+          <NuxtLink :to="item.path" v-else>{{ item.text }}</NuxtLink>
+        </li>
+      </template>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  margin-bottom: 1em;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.5em;
+  list-style: none;
+}
+
+li {
+  &:not(:first-child) {
+    &::before {
+      content: '>';
+      margin-right: 0.5em;
+    }
+  }
+}
+</style>
---
+++ components/pageTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1><slot/></h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  @include sp {
+    font-size: 1.5em;
+  }
+  @include pc {
+    font-size: 2em;
+  }
+}
+</style>
