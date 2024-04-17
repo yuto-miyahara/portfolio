dc26d6c18a7b3a917a7d62e481150974aab3b87e
yuto-miyahara <yt.miyahara@gmail.com>
2024-04-17 18:24:12 +0900

    プロフィールページの略歴のデータを外部化

---
+++ components/history/historyBlock.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  head: String,
+  body: String,
+})
+</script>
+
+<template>
+  <section>
+    <h3 class="historyBlock__head">{{ head }}</h3>
+
+    <div class="historyBlock__body" v-html="body"></div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+h3 {
+  font-weight: bold;
+  @include pc {
+    font-size: 1.25em;
+  }
+}
+</style>
