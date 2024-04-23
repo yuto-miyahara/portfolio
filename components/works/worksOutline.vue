a4b60fbef15b4ef8d6d082e155cbd2c8438c8227
yuto-miyahara <yt.miyahara@gmail.com>
2024-04-24 01:04:37 +0900

    制作事例ページを仮作成

---
+++ components/works/worksOutline.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  data: Object,
+})
+</script>
+
+<template>
+  <div class="outline">
+    <template v-for="value, key in data" :key="key">
+      <div class="outline__block" v-if="value">
+        <p class="outline__head">{{ key }}</p>
+        <p class="outline__body">{{ value }}</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.outline {
+  &__block {
+    display: flex;
+    gap: 1em;
+  }
+
+  &__head {
+    width: 4em;
+    font-weight: bold;
+    text-align: left;
+  }
+}
+</style>
---
+++ pages/works.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const pageTitle: string = '制作事例'
+
+const bcParam = [
+  {
+    text: pageTitle,
+    path: null,
+  },
+]
+</script>
+
+<template>
+  <breadcrumb :bcParam/>
+
+  <PageTitle>{{ pageTitle }}</PageTitle>
+
+  <pageContent>
+    <p>これまでに作成してきたサイトの情報をまとめています。</p>
+
+    <WorksList/>
+  </pageContent>
+</template>
+
+<style lang="scss" scoped>
+</style>
